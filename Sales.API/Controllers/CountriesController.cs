8599e9cb6c4b6906b923e774f26ee310ab6110d3
Ing-Samuel <samuelhz1998@gmail.com>
2023-02-24 22:48:52 -0500

    Create All Projects and Complete Api Countries

---
+++ Sales.API/Controllers/CountriesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Controllers
+{
+    [ApiController]
+    [Route("/api/countries")]
+    public class CountriesController:ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAsync()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Country country)
+        {
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x=>x.Id==id);
+            if(country is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(country);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Country country)
+        {
+            _context.Countries.Update(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var afectedRow = await _context.Countries.Where(x=>x.Id==id).ExecuteDeleteAsync();
+            if(afectedRow == 0)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+    }
+}
---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener por lo menos 100 carácteres")]
+        [Display(Name = "País")]
+        public string Name { get; set; } = null!;
+    }
+}
