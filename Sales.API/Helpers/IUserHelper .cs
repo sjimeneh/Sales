c63299959cc159298f7934f90351b3a1808ac99a
Ing-Samuel <samuelhz1998@gmail.com>
2023-03-20 22:13:53 -0500

    I created the Edit info user

--- Sales.API/Helpers/IUserHelper .cs
+++ Sales.API/Helpers/IUserHelper .cs
@@ -19,6 +19,13 @@ namespace Sales.API.Helpers
 
         Task LogoutAsync();
 
+        Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword);
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+        Task<User> GetUserAsync(Guid userId);
+
+
 
     }
 }
